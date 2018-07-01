1461442314ced992e6a8e31a772861c657882261
Martin Røvang <martinrovang@gmail.com>
2018-07-02 00:56:15 +0200

    Tweaks

--- deploy.py
+++ deploy.py
@@ -68,32 +68,24 @@ def main():
     try:
         conn = sql.connect("twitter.db", check_same_thread=False)
         df = pd.read_sql("SELECT * FROM sentiment_fts fts LEFT JOIN sentiment ON fts.rowid = sentiment.id WHERE fts.sentiment_fts MATCH ? ORDER BY fts.rowid DESC LIMIT 1000", conn, params=(sentiment_term+'*',))
-    # colnames = table_columns(conn, 'sentiment_fts')
-    # df = pd.read_sql(
-    #     "SELECT * FROM sentiment_fts " + 
-    #     "sentiment_fts LEFT JOIN sentiment ON sentiment_fts.rowid = sentiment.id" + 
-    #     "WHERE sentiment_fts.tweet MATCH ?" + 
-    #     "ORDER BY sentiment_fts.rowid DESC LIMIT 100", conn,)
-        print(df['sentiment'])
-    # df.sort_values('unix', inplace=True)
-    # df['date'] = pd.to_datetime(df['unix'], unit='ms')
-    # df.set_index('date', inplace=True)
-    # init_length = len(df)
-    # df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
-    # df = df_resample_sizes(df)
-    # X = df.index
-    # Y = df.sentiment_smoothed.values
-    # Y2 = df.volume.values
-    # if Y[0] > 0:
-    #     return render_template("oppover.html",Yverdi = Y[0])
+        df.sort_values('unix', inplace=True)
+        df['date'] = pd.to_datetime(df['unix'], unit='ms')
+        df.set_index('date', inplace=True)
+        init_length = len(df)
+        df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+        df = df_resample_sizes(df)
+        X = df.index
+        Y = df.sentiment_smoothed.values
+        Y2 = df.volume.values
+        if Y[0] > 0:
+            return render_template("oppover.html",Yverdi = Y[0])
 
-    # else:
-    #     return render_template("nedover.html",Yverdi = Y[0])
+        else:
+            return render_template("nedover.html",Yverdi = Y[0])
     except Exception as e:
         with open('errors.txt','a') as f:
             f.write(str(e))
             f.write('\n')
-    return('test')
 
 
 
